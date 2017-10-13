bb1cf2456883197d33335cbba6997a2c79c3682f
Ruba Rammal <ruba.ramm@gmail.com>
2017-10-13 17:07:18 +0300

    Version 1

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Neighborhood Map
+## Neighborhood Map project
+
+This project is part of Udacity's frontend nanodegree. The goal is to create a map that have several locations and a search field to filter the locations.
+
+### Getting started
+
+* Click [here](https://rubarammal.github.io/Neighborhood-Map/) to visit the Café Locator website. Or clone the project and open `index.html`.
+* Locations on the map can be filtered using the search field on the sidebar.
+* Clicking a marker on the map opens a window that displays the location's name and a photo from that location.
+* The hamburger icon on the top bar hides and shows the sidebar that contains the locations' list.
+
+### Dependencies
+
+__* This project uses the following APIs:__
+
+1. [Google Maps APIs](https://developers.google.com/maps/) 
+This API is used to load a map, create markers of locations on the map and opens an infoWindow that displays location details when a marker is clicked.
+1. [Foursquare Places API](https://developer.foursquare.com/places-api)
+This API is used to get venues near a specific location and it is also used to get photos of these venues.
--- js/app.js
+++ js/app.js
@@ -8,9 +8,19 @@
      var locations = [];
      var viewModel;
 
-     var result;
+     var places = [];
 
+     /**
+      * Location object
+      */
+     var Location = function(name, marker) {
+         this.name = ko.observable(name);
+         this.marker = marker;
+     };
 
+     /**
+      * @description Initializes the map and requests locations from Foursquare API
+      */
      function initMap() {
 
          markerIcon = makeMarkerIcon('b20000');
@@ -20,49 +30,112 @@
              lat: 24.7113143,
              lng: 46.6722581
          };
-         map = new google.maps.Map(document.getElementById('map'), {
-             center: mainLoc,
-             zoom: 16
+         // Check if map is loaded succefully
+         if (typeof google === 'object' && typeof google.maps === 'object') {
+             map = new google.maps.Map(document.getElementById('map'), {
+                 center: mainLoc,
+                 zoom: 16
+             });
+
+             infoWindow = new google.maps.InfoWindow();
+             // Get locations (coffee stores) from Foursquare API that are within 500 radius of the main location
+             var url = 'https://api.foursquare.com/v2/venues/search?ll=' + mainLoc.lat + ',' + mainLoc.lng +
+                 '&query=coffee&radius=500&client_id=PVARFLB4RALKAWI0SHBUSM3GPAHX3B02L3YGX0R3IOV2NCQO&client_secret=SY334RFX554W34D5S5CEX3YUR3VT1T2OSXAR5CDRVKW3CFAO&v=20170304';
+
+             $.ajax({
+                 url: url,
+                 dataType: 'json',
+                 success: function(data) { // Request successful --> create location on map
+                     for (var i = 0; i < places.response.venues.length; i++) {
+                         createLocation(places.response.venues[i]);
+                     }
+                 },
+                 error: function(data) { // Request fail --> display error message
+                     $('.error-text').text('An error occured while loading the locations!');
+                     $('#error-block').show();
+                     setTimeout(function() {
+                         $('.error-text').text('');
+                         $('#error-block').hide();
+                     }, 5000);
+                 }
+             });
+         } else { // Error handling map load
+             $('.error-text').text('An error occured while loading the map!');
+             $('#error-block').show();
+             setTimeout(function() {
+                 $('.error-text').text('');
+                 $('#error-block').hide();
+             }, 5000);
+         }
+
+         viewModel = new ViewModel(locations);
+         ko.applyBindings(viewModel);
+     }
+
+     /**
+      * @description Takes a venue and requests photos of that venue's id 
+      * from the Foursquare API and adds it to the infoWindow 
+      * Creates a marker for the venue and adds a click event 
+      * to it that displays the infoWindow details
+      * @param {venues []} venue
+      */
+     function createLocation(venue) {
+
+         // Get venue photos from the venue id
+         var url = 'https://api.foursquare.com/v2/venues/' + venue.id +
+             '/photos?client_id=PVARFLB4RALKAWI0SHBUSM3GPAHX3B02L3YGX0R3IOV2NCQO&client_secret=SY334RFX554W34D5S5CEX3YUR3VT1T2OSXAR5CDRVKW3CFAO&v=20170304';
+         var photos;
+         var photo;
+
+         $.ajax({
+             url: url,
+             dataType: 'json',
+             success: function(data) { // Request succefull --> choose the index of one random photo from the photos array
+                 photos = data.response.photos.items;
+
+                 if (photos.length > 0) {
+                     var randomPhoto = Math.floor(Math.random() * ((photos.length - 1) - 0 + 1) + 0);
+                     photo = photos[randomPhoto].prefix + (photos[randomPhoto].width * 0.1) + 'x' +
+                         (photos[randomPhoto].height * 0.1) + photos[randomPhoto].suffix;
+                 }
+             },
+             error: function(data) { // Request fail --> display error message
+                 $('.error-text').text('An error occured while loading location photos!');
+                 $('#error-block').show();
+                 setTimeout(function() {
+                     $('.error-text').text('');
+                     $('#error-block').hide();
+                 }, 5000);
+
+             }
          });
 
-         infowindow = new google.maps.InfoWindow();
-         var service = new google.maps.places.PlacesService(map);
-         service.nearbySearch({
-             location: mainLoc,
-             radius: 1000,
-             type: ['cafe']
-         }, callback);
-     }
-
-     function callback(results, status) {
-
-         result = results;
-         if (status === google.maps.places.PlacesServiceStatus.OK) {
-             for (var i = 0; i < results.length; i++) {
-                 createLocation(results[i]);
-             }
-             viewModel = new ViewModel(locations);
-             ko.applyBindings(viewModel);
-         } //TODO: handle error
-     }
-
-     function createLocation(place) {
-         var placeLoc = place.geometry.location;
+         // Create marker
          var marker = new google.maps.Marker({
              map: map,
-             position: place.geometry.location,
+             position: venue.location,
              icon: markerIcon
          });
 
-         locations.push(new Location(place.name, marker));
+         // Push an instance of the Location object to the locations array
+         locations.push(new Location(venue.name, marker));
 
+         // Add click event listener to the marker
+         // It appends the photo and name to the infoWindow and opens it
          google.maps.event.addListener(marker, 'click', function() {
              marker.setIcon(markerIconHover);
-             infowindow.setContent(place.name);
-             infowindow.open(map, this);
+
+             if (photo) infoWindow.setContent('<div>' + venue.name + '<br><img src="' + photo + '"></div>');
+             else infoWindow.setContent('<div>' + venue.name + '</div>');
+             infoWindow.open(map, this);
          });
      }
 
+     /**
+      * @description Creates a marker icon
+      * @param {String} markerColor
+      * @return {String} image
+      */
      function makeMarkerIcon(markerColor) {
          var image = {
              url: 'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor + '|40|_|%E2%80%A2',
@@ -74,41 +147,48 @@
          return image;
      }
 
-     // represent a single location item
-     var Location = function(name, marker) {
-         this.name = ko.observable(name);
-         this.marker = marker;
-     };
-
+     /**
+      * @description Handles displaying the locations in list view 
+      * and filtering the locations based on the input from the 
+      * text field and handles list item (location) clicks
+      * @param {Location []} loc
+      */
      var ViewModel = function(loc) {
          var self = this;
+         self.title = ko.observable('Café Locator');
+         self.api = ko.observable('Foursquare API');
          self.places = ko.observableArray(loc);
          self.navVisible = ko.observable(false);
+         // Toggle class to hide/show sidebar
          self.toggleSidebar = function(data, event) {
-             self.navVisible(!self.navVisible()); //toggle the navVisible value between true/false
-         }
+             self.navVisible(!self.navVisible());
+         };
+         // Trigger marker click when a location is clicked
          self.placeClick = function(place) {
              google.maps.event.trigger(place.marker, 'click');
-         }
+         };
          self.filterLoc = ko.observable('');
+         // Filter locations
          self.filterLocations = ko.computed(function() {
              var filter = self.filterLoc().toLowerCase();
              var tempLocations;
 
              if (!filter) {
                  ko.utils.arrayForEach(self.places(), function(temp) {
-                     temp.marker.setVisible(true);
+                     temp.marker.setVisible(true); // if text field is clear --> show all markers
                  });
-                 return self.places();
+                 return self.places(); // return all locations
              } else {
+                 // filtered locations
                  tempLocations = ko.utils.arrayFilter(self.places(), function(item) {
-                     item.marker.setVisible(false);
+                     item.marker.setVisible(false); // hide all markers
                      return item.name().toLowerCase().indexOf(filter) !== -1;
                  });
              }
+             // show markers of filtered locations
              ko.utils.arrayForEach(tempLocations, function(temp) {
                  temp.marker.setVisible(true);
              });
-             return tempLocations;
+             return tempLocations; // return filtered locations
          });
      };
